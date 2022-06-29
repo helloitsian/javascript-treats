f11fd03c8566be6e943eb813f2060868293753a9
helloitsian <15301476+helloitsian@users.noreply.github.com>
2022-06-29 01:12:24 -0500

    renamed, cleaned up comments

--- misc/createEventListenerStore.js
+++ misc/createEventListenerStore.js
@@ -1,40 +1,34 @@
-const createEventHandlerStore = () => {
+const createEventListenerStore = () => {
   const store = new Map();
+  // holds our entries in memory so we can easily refence listeners in our Map
   let entries = [];
 
   return [
-    // add handler
-    (target, event, handler) => {
+    // add listener
+    (target, event, listener) => {
       const entry = { target, event };
-      store.set(entry, handler);
       entries.push(entry);
-      target.addEventListener(event, handler);
+      store.set(entry, listener);
+      target.addEventListener(event, listener);
     },
-
-    // remove handler
+    // remove listener
     (target, event) => {
     	for (let i = 0; i < entries.length; i++) {
       	if (JSON.stringify({ target,event }) === JSON.stringify(entries[i])) {
           target.removeEventListener(event, store.get(entries[i]));
           store.delete(entries[i]);
+          entries.splice(i, 1);
         }
      	}
-      
     },
-    
-    // remove all handlers
+    // remove all listeners
     () => {
     	// iterate our entries, so we can iterate our Map key value pairs
       for (let i = 0; i < entries.length; i++) {
-      	// get our handler using our entry
-
         entries[i].target.removeEventListener(entries[i].event, store.get(entries[i]));
       }
-      // clear entries
       entries = [];
     },
     store,
   ]
-} 
-
-// USAGE: https://jsfiddle.net/mgcpes0w/7/
+}
